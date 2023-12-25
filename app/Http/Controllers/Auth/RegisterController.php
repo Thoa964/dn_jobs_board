632cbe2e23b055c33ba99527f68c01b9403d4b68
Thoa964 <84794644+Thoa964@users.noreply.github.com>
2023-12-25 14:26:06 +0700

    FixBug

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -59,7 +59,7 @@ class RegisterController extends Controller
     {
         $data = CompanyRegisterData::fromRequest($request);
 
-        $companyData = $this->crawlDataProcessing($data['ma_so_thue']);
+        $companyData = $this->crawlDataProcessing($data['ma_so_thue'], $this->vn_to_str($data['ten_cong_ty']));
 
         if (!$this->validateTenCongTy(
             $data['ten_cong_ty'],
@@ -125,9 +125,9 @@ class RegisterController extends Controller
         return $requestName == $tenNguoiDaiDien;
     }
 
-    private function crawlDataProcessing(string $maSoThue): array {
-
-        $response = $this->client->request("GET", "https://masothue.com/Search?q=$maSoThue&token=thoa2503&type=enterprise");
+    private function crawlDataProcessing(string $maSoThue, string $pathTenCongTy): array {
+        $path = strtolower("$maSoThue-$pathTenCongTy");
+        $response = $this->client->request("GET", "https://masothue.com/$path");
         $tenNguoiDaiDien = $response->filter('td > span[itemprop="name"]')->text();
         $soDienThoai = $response->filter('td[itemprop="telephone"]')->text();
         $tenCongTy = $response->filter('h1[class="h1"]')->text();
@@ -169,5 +169,33 @@ class RegisterController extends Controller
             'ma_quyen' => 2,
             'trang_thai' => 1
         ]);
+    }
+
+    private function vn_to_str($str) {
+ 
+        $unicode = array(
+            'a'=>'á|à|ả|ã|ạ|ă|ắ|ặ|ằ|ẳ|ẵ|â|ấ|ầ|ẩ|ẫ|ậ',
+            'd'=>'đ',
+            'e'=>'é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ',
+            'i'=>'í|ì|ỉ|ĩ|ị',
+            'o'=>'ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ',
+            'u'=>'ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự',
+            'y'=>'ý|ỳ|ỷ|ỹ|ỵ',
+            'A'=>'Á|À|Ả|Ã|Ạ|Ă|Ắ|Ặ|Ằ|Ẳ|Ẵ|Â|Ấ|Ầ|Ẩ|Ẫ|Ậ',
+            'D'=>'Đ',
+            'E'=>'É|È|Ẻ|Ẽ|Ẹ|Ê|Ế|Ề|Ể|Ễ|Ệ',
+            'I'=>'Í|Ì|Ỉ|Ĩ|Ị',
+            'O'=>'Ó|Ò|Ỏ|Õ|Ọ|Ô|Ố|Ồ|Ổ|Ỗ|Ộ|Ơ|Ớ|Ờ|Ở|Ỡ|Ợ',
+            'U'=>'Ú|Ù|Ủ|Ũ|Ụ|Ư|Ứ|Ừ|Ử|Ữ|Ự',
+            'Y'=>'Ý|Ỳ|Ỷ|Ỹ|Ỵ',
+        );
+         
+        foreach($unicode as $nonUnicode=>$uni){
+            $str = preg_replace("/($uni)/i", $nonUnicode, $str);
+        }
+
+        $str = str_replace(' ','-',$str);
+         
+        return $str;
     }
 }
